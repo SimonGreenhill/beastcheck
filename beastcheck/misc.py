babbb84252a57c854e7bcfb13dfe77ae99fa9361
SimonGreenhill <simon@simon.net.nz>
2016-09-29 16:38:06 +0200

    checks ascertainment correction better. Fixes #2

--- beastcheck/misc.py
+++ beastcheck/misc.py
@@ -4,18 +4,29 @@
 class AscertainmentBias(object):
     """Mixin to test for Ascertainment Bias"""
     def test_ascertainment_character(self):
-        for seq in self.xml.findall('./data/sequence'):
-            site_zero = seq.get('value')[0]
-            if site_zero != '0':
-                raise AssertionError(
-                    "Expected site zero to be 0 for ascertainment"
-                )
-    
+        sequences = self.xml.findall('./data/sequence')
+        p = './/distribution[@id="likelihood"]/distribution/data/data'
+        for part in self.xml.findall(p):
+            try:
+                site = int(part.get('filter').split("-")[0])
+            except:
+                print("Invalid filter %r for %s" % (part.get('filter'), part.get('id')))
+                raise
+
+            for seq in sequences:
+                site_zero = seq.get('value')[0]
+                if site_zero not in ('0', '?', '-'):
+                    raise AssertionError(
+                        "Expected site zero to be 0/?/- for ascertainment"
+                    )
+
     def test_treeLikelihood_corrects_for_ascertainment(self):
-        data = self.xml.find('.//distribution[@id="likelihood"]//data')
-        assert data.get('ascertained') == 'true'
-        
+        p = './/distribution[@id="likelihood"]/distribution/data'
+        for data in self.xml.findall(p):
+            assert data.get('ascertained') == 'true'
+
     def test_treeLikelihood_has_exclude_set_correctly(self):
-        data = self.xml.find('.//distribution[@id="likelihood"]//data')
-        assert data.get('excludeto') == '1'  # not inclusive.
-      
+        p = './/distribution[@id="likelihood"]/distribution/data'
+        for data in self.xml.findall(p):
+            # not inclusive
+            assert data.get('excludeto') == '1', "%s does not have exclude set to 1" % data.get('id')
